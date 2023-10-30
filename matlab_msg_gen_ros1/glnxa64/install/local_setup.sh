bd4462a6d589ef749d02ded2f5f4e65b23fca136
Joshua Impey <joshimpey10@gmail.com>
2023-10-30 14:02:23 +1100

    Adding to gui

--- matlab_msg_gen_ros1/glnxa64/install/local_setup.sh
+++ matlab_msg_gen_ros1/glnxa64/install/local_setup.sh
@@ -3,7 +3,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/mitch/catkin_ws/src/fishBotBattle/matlab_msg_gen_ros1/glnxa64/install}
+: ${_CATKIN_SETUP_DIR:=/home/josh/Git/fishBotBattle/matlab_msg_gen_ros1/glnxa64/install}
 CATKIN_SETUP_UTIL_ARGS="--extend --local"
 . "$_CATKIN_SETUP_DIR/setup.sh"
 unset CATKIN_SETUP_UTIL_ARGS
