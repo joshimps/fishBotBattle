ee780a556a7806ccd126ea90ed02cda6aaa3af5a
Mitchell Rosser <69025240+sheepskins@users.noreply.github.com>
2023-10-31 17:34:01 +1100

    Delete matlab_msg_gen_ros1 directory
    This folder needs to be generated locally

--- matlab_msg_gen_ros1/glnxa64/install/share/fishbot_ros/cmake/fishbot_ros-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(fishbot_ros_MESSAGE_FILES "")
-set(fishbot_ros_SERVICE_FILES "srv/chess_service.srv")
--- matlab_msg_gen_ros1/glnxa64/install/share/fishbot_ros/cmake/fishbot_ros-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
-
-_prepend_path("${fishbot_ros_DIR}/.." "" fishbot_ros_MSG_INCLUDE_DIRS UNIQUE)
-set(fishbot_ros_MSG_DEPENDENCIES std_msgs)
